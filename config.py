4a9623c9907d184ffdfbcdc1df68321c96fe3ac2
Andrii Fylypiuk <fakeandrii@icloud.com>
2022-11-15 15:37:33 +0100

    reformated config.py a bit

--- config.py
+++ config.py
@@ -1,17 +1,17 @@
 # Default configuration for the project.
 
 
-api_id = 0 # Your API ID from Telegram API Setting, as an integer
+api_id = None # Your API ID from Telegram API Settings, as an integer
 
-api_hash = "" # Your API HASH from Telegram API Setting, as a string
+api_hash = None # Your API HASH from Telegram API Settings, as a string
 
 api_sessions_dir = "./sessions" # Absolute or relative path to the directory, where sessions would be stored, do not write / at the end, if it is possible, do not change this parameter
 
 api_session_name = "default" # Session name, should be as a valid filename in your system
 
-font = ("font/defaults/digit.ttf", 128) # Font settings, tuple consists of:
+font = ("./font/defaults/digit.ttf", 128) # Font settings, tuple consists of:
 # - Path to file with the font, allowed only TrueType fonts, as a string
-# - Font size, as an integer
+# - Font size, in points (pt), as an integer
 
 color = ((0, 0, 0), (255, 255, 255)) # Color settings, tuple consists of:
 # - Background color in numerical RGB format (each number is 0..255)
@@ -24,6 +24,6 @@ dimensions = (512, 512) # Photo dimensions settings, tuple consists of:
 
 time_format = "%H:%M" # String, a Python date format (described at https://docs.python.org/3/library/datetime.html#strftime-and-strptime-format-codes), which will be used in order to generate image
 
-cache_directory = "cache" # Directory name used for the project's cache, if it is possible, do not change this parameter
+cache_directory = "./cache" # Directory name used for the project's cache, do not write / at the end, if it is possible, do not change this parameter
 
 image_filename = "image" # Filename of generated image, please don't use pathes and file extensions, if it is possible, do not change this parameter
